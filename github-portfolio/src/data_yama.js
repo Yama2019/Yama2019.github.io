e69dcdbe85c8e4aa2e4710dca11fe4b337fb0daf
Yamil <50251504+Yama2019@users.noreply.github.com>
2021-12-22 11:31:11 -0300

    initial commit
    Primer commit

---
+++ github-portfolio/src/data_yama.js
@@ -0,0 +1,158 @@
+module.exports = {
+    "data": {
+        "header":{
+            "name":"YAMIL WALTER ALI",
+            "profession": "Software Developer",
+            "location": {
+                "country": "Argentina",
+                "dapartament": "Buenos Aires",
+                "city": "Pergamino"
+            }
+        },
+        "body":{
+            "principal":{
+                "resume":"",
+                "professionalExperience":[
+                    {
+                        "title": "Experienced Developer",
+                        "date": "09/2019 - Actual",
+                        "entity": "Rojas, Buenos Aires",
+                        "activities":[
+                            "Desarrollador en la consultora Neoris"
+                        ]
+                    },
+                    {
+                        "title": "BBVA",
+                        "date": "10/2019",
+                        "entity": "Neoris",
+                        "activities":[
+                            "Desarrollo de un formulario utilizando VueJs para el front-end y Java Spring boot para el back-end."
+                        ]
+                    },
+                    {
+                        "title": "Banco Comafi",
+                        "date": "03/2020",
+                        "entity": "Neoris",
+                        "activities":[
+                            "Mantenimiento y desarrollo con Angular 7 y Java Spring Boot."
+                        ]
+                    },
+                    {
+                        "title": "Direct Tv/ TTW",
+                        "date": "07/2020 -11/2021",
+                        "entity": "Neoris",
+                        "activities":[
+                            "Desarrollo y modificaciones de reglas de negocio mediante Drools (Red Hat) utilizando el lenguaje Java. Mantenimiento y configuracion de servidores"
+                        ]
+                    },
+                    {
+                        "title": "Personal",
+                        "date": "11/2021 -Actual",
+                        "entity": "Neoris",
+                        "activities":[
+                            "Desarrollo con NodeJs del backoffice"
+                        ]
+                    },
+                    {
+                        "title": "App mobile para almacenar contraseñas",
+                        "date": "08/2019",
+                        "entity": "Freelance",
+                        "activities":[
+                            "Desarrollo de un app mobile para almacenar contraseñas, utilizando Ionic, NodeJs y Mongodb"
+                        ]
+                    }
+                ],
+                "education":[
+                    {
+                        "title":"Técnico Universitario en programación",
+                        "entity": "Universidad Tecnologica Nacional",
+                        "date":"2017-2019",
+                        "city": "Rojas, Buenos Aires"
+                    },
+                    {
+                        "title":"Contador Publico",
+                        "entity": "UNNOBA",
+                        "date":"sin finalizar",
+                        "city": "Pergamino, Buenos Aires"
+                    },
+                    {
+                        "title":"Tramo de formación pedagogica",
+                        "entity": "UFASTA",
+                        "date":"finalizado",
+                        "city": "Mar del plata"
+                    },
+                ],
+                "hobbies":[
+                    "Aprender nuevas tecnologias y desarrollar habilidades", 
+                     
+                ]
+            },
+            "complementary":{
+                "personalInfo":{
+                    "mobile": "(2474) 680163",
+                    "email": "yamil.ali.1995@gmail.com",
+                    "id": {
+                        "number": "2474 689163",
+                        "city": "Rojas"
+                    },
+                    "git":"",
+                    "linkedin": "https://www.linkedin.com/in/yamil-ali-71199a183/"
+                },
+                "hardSkills":[
+                    {
+                        "name": "Angular",
+                        "level": 4
+                    },
+                    {
+                        "name": "Java",
+                        "level": 4
+                    },
+                    {
+                        "name": "ReactJs",
+                        "level": 3
+                    },
+                    {
+                        "name": "NodeJs",
+                        "level": 3
+                    },
+                    {
+                        "name": "Mysql",
+                        "level": 2
+                    },
+                    {
+                        "name": "Html",
+                        "level": 4
+                    },
+                    {
+                        "name": "CSS,CSS3,CSS4",
+                        "level": 4
+                    },
+                    {
+                        "name": "Javascript,Es5,Es6",
+                        "level": 4
+                    },
+                ],
+                "softSkills":[
+                    {
+                        "name":"Responsabilidad",
+                        "level":5
+                    },
+                    {
+                        "name":"Trabajo en equipo",
+                        "level": 4
+                    },
+                    {
+                        "name":"Comunicación",
+                        "level": 4,
+                    }
+                ],
+                "languages":[
+                    {
+                        "name":"English",
+                        "level": 3
+                    },
+                ]
+            }
+        }
+    }
+}
